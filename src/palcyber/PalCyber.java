7727f9bf33191c6e225adbb50a36d79bde8e23d0
EnderPC <EnderPC@DESKTOP-SAKO7OI>
2022-06-20 21:55:43 -0600

    prueba

---
+++ src/palcyber/PalCyber.java
@@ -0,0 +1,8 @@
+package palcyber;
+public class PalCyber {
+
+    public static void main(String[] args) {
+        System.out.println("El Carpiño, Capibara o Chigüire es el roedor más grande del mundo y habita los llanos venezolanos ");
+    }
+    
+}
